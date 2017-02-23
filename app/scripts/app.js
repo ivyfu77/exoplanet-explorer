a89762ea77b62a7de9a03ac8d9340feb6c46d008
Ivy Fu <ivy@hmsnz.com>
2017-02-23 16:26:17 +1300

    Complete the quiz task

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -36,22 +36,28 @@ Instructions:
     /*
     This code needs to get wrapped in a Promise!
      */
-    var req = new XMLHttpRequest();
-    req.open('GET', url);
-    req.onload = function() {
-      if (req.status === 200) {
-        // It worked!
-        // You'll want to resolve with the data from req.response
-      } else {
+    return new Promise(function(resolve, reject){
+      var req = new XMLHttpRequest();
+      req.open('GET', url);
+      req.onload = function() {
+        if (req.status === 200) {
+          // It worked!
+          // You'll want to resolve with the data from req.response
+          resolve(req.response);
+        } else {
+          // It failed :(
+          // Be nice and reject with req.statusText
+          reject(req.statusText);
+        }
+      };
+      req.onerror = function() {
         // It failed :(
-        // Be nice and reject with req.statusText
-      }
-    };
-    req.onerror = function() {
-      // It failed :(
-      // Pass a 'Network Error' to reject
-    };
-    req.send();
+        // Pass a 'Network Error' to reject
+        reject('Network Error');
+      };
+      req.send();
+
+    });
   }
 
   window.addEventListener('WebComponentsReady', function() {
@@ -61,6 +67,19 @@ Instructions:
     You'll need to add a .then and a .catch. Pass the response to addSearchHeader on resolve or
     pass 'unknown' to addSearchHeader if it rejects.
      */
-    // get('../data/earth-like-results.json')
+    get('../data/earth-like-results00.json')
+      .then(function(response) {
+        console.log(response);
+        let results = JSON.parse(response).results;
+        console.log(results);
+        for(let item of results) {
+          console.log(item);
+        }
+        addSearchHeader(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+        addSearchHeader('unknown');
+      });
   });
 })(document);
