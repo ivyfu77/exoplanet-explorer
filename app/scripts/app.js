14d54cdedf4a25f5c09a16db2d7ae3d92e73d720
Ivy Fu <ivy@hmsnz.com>
2017-02-24 14:18:32 +1300

    Promise with .map

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -59,6 +59,21 @@ Instructions:
     Your code goes here! Uncomment the next line when you're ready to start!
      */
 
-    // getJSON('../data/earth-like-results.json')
+    getJSON('../data/earth-like-results.json')
+    .then(function(response) {
+      // var sequence = Promise.resolve();
+      // response.results.map(function(url) {
+      //   sequence = sequence.then(function() {
+      //     return getJSON(url);
+      //   })
+      //   .then(createPlanetThumb);
+      // });
+      response.results.map(function(url){
+        getJSON(url).then(createPlanetThumb);
+      })
+    })
+    .catch(function(e) {
+      console.log(e);
+    })
   });
 })(document);
